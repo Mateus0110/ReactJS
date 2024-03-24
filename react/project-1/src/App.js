aca460e854f4371fe593f5b4851ec3dafbc0a383
Enzo Evangelista <enzoteu55@gmail.com>
2024-03-23 22:11:17 -0300

    Stateful class components (Without class fields and with bind)

--- react/project-1/src/App.js
+++ react/project-1/src/App.js
@@ -3,47 +3,57 @@ import "./App.css";
 import logo from "./logo.svg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this); // BIND FUNCTION HERE
+
+    this.state = {
+      name: 'Enzo Evangelista',
+      counter: 0,
+    };
+  }
+
+  handlePClick() {
+    // const { name } = this.state; // Without performing the bind above, it will not be possible to access the class of the class
+    // window.alert(name);
+
+    this.setState({
+      name: 'Júnior'
+    });
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+
+    this.setState({
+      counter: counter + 1
+    });
+  }
+
   render() {
+    const { name, counter } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            { name } { counter }
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Esté é o link
           </a>
         </header>
       </div>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
